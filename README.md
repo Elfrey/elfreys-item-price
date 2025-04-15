de9c0601af865f3521946c2d1440d46dc5eaac2e
Elfery <elfrey@mail.ru>
2025-04-15 16:23:01 +0500

    Rewrite to performance improvements

--- README.md
+++ README.md
@@ -1,71 +1,26 @@
-![](https://img.shields.io/badge/Foundry-v10-informational)
-<!--- Downloads @ Latest Badge -->
-<!--- replace <user>/<repo> with your username/repository -->
-<!--- ![Latest Release Download Count](https://img.shields.io/github/downloads/<user>/<repo>/latest/module.zip) -->
+![](https://img.shields.io/badge/Foundry-v12-informational)
+![Latest Release Download Count](https://img.shields.io/github/downloads/elfrey/elfrey-mc-recipes/latest/module.zip)
 
-<!--- Forge Bazaar Install % Badge -->
-<!--- replace <your-module-name> with the `name` in your manifest -->
-<!--- ![Forge Installs](https://img.shields.io/badge/dynamic/json?label=Forge%20Installs&query=package.installs&suffix=%25&url=https%3A%2F%2Fforge-vtt.com%2Fapi%2Fbazaar%2Fpackage%2F<your-module-name>&colorB=4aa94a) -->
+# 🎲 Модуль «Генератор Цены» для D&D 5e
 
 
-# How to use this Template to create a versioned Release
+## 📌 Описание
 
-1. Open your repository's releases page.
+**«Генератор Цены»** — небольшой модуль, автоматически генерирующий цену предметов в зависимости от их редкости для системы **D&D 5e** (версия 4.3.1+).
 
-![Where to click to open repository releases.](https://user-images.githubusercontent.com/7644614/93409301-9fd25080-f864-11ea-9e0c-bdd09e4418e4.png)
+При импорте предметов из библиотеки цена автоматически рассчитывается. Если предмет является расходником (например, боеприпасы, свитки заклинаний или зелья), то его базовая цена делится на два.
 
-2. Click "Draft a new release"
+## ⚙️ Настройка и использование
+![Настройка](img.png)
 
-![Draft a new release button.](https://user-images.githubusercontent.com/7644614/93409364-c1333c80-f864-11ea-89f1-abfcb18a8d9f.png)
+1. **Метод генерации цен**  
+   В настройках модуля можно выбрать желаемый метод генерации цен. В будущем планируется добавить возможность указания собственных формул и фиксированных значений.
 
-3. Fill out the release version as the tag name.
-
-If you want to add details at this stage you can, or you can always come back later and edit them.
-
-![Release Creation Form](https://user-images.githubusercontent.com/7644614/93409543-225b1000-f865-11ea-9a19-f1906a724421.png)
-
-4. Hit submit.
-
-5. Wait a few minutes.
-
-A Github Action will run to populate the `module.json` and `module.zip` with the correct urls that you can then use to distribute this release. You can check on its status in the "Actions" tab.
-
-![Actions Tab](https://user-images.githubusercontent.com/7644614/93409820-c1800780-f865-11ea-8c6b-c3792e35e0c8.png)
-
-6. Grab the module.json url from the release's details page.
-
-![image](https://user-images.githubusercontent.com/7644614/93409960-10c63800-f866-11ea-83f6-270cc5d10b71.png)
-
-This `module.json` will only ever point at this release's `module.zip`, making it useful for sharing a specific version for compatibility purposes.
-
-7. You can use the url `https://github.com/<user>/<repo>/releases/latest/download/module.json` to refer to the manifest.
-
-This is the url you want to use to install the module typically, as it will get updated automatically.
-
-# How to List Your Releases on Package Admin
-
-To request a package listing for your first release, go to the [Package Submission Form](https://foundryvtt.com/packages/submit) (accessible via a link at the bottom of the "[Systems and Modules](https://foundryvtt.com/packages/)" page on the Foundry website).
-
-Fill in the form. "Package Name" must match the name in the module manifest.  Package Title will be the display name for the package.  Package URL should be your repo URL.
-![image](https://user-images.githubusercontent.com/36359784/120664263-b49e5500-c482-11eb-9126-af7006389903.png)
+2. **Выбор библиотек** *(важно!)*  
+   Обязательно перейдите в раздел **«Выбрать библиотеки»** и отметьте те, к которым должны применяться новые цены.
 
 
-One of the Foundry staff will typically get back to you with an approval or any further questions within a few days, and give you access to the package admin pages.
+## 🐞 Обратная связь
 
-Once you have access to the [module admin page](https://foundryvtt.com/admin/packages/package/), you can release a new version by going into the page for your module, scrolling to the bottom, and filling in a new Package Version.
+Если у вас есть предложения по улучшению или вы обнаружили ошибки, буду рад вашим замечаниям и рекомендациям. Пожалуйста, создавайте Issue на GitHub или свяжитесь со мной напрямую.
 
-When listing a new version, Version should be the version number you set above, and the Manifest URL should be the manifest __for that specific version__ (do not use /latest/ here).
-![image](https://user-images.githubusercontent.com/36359784/120664346-c4b63480-c482-11eb-9d8b-731b50d70939.png)
-
-> ### :warning: Important :warning:
-> 
-> It is very important that you use the specific release manifest url, and not the `/latest` url here. For more details about why this is important and how Foundry Installs/Updates packages, read [this wiki article](https://foundryvtt.wiki/en/development/guides/releases-and-history).
-
-Clicking "Save" in the bottom right will save the new version, which means that anyone installing your module from within Foundry will get that version, and a post will be generated in the #release-announcements channel on the official Foundry VTT Discord.
-
-
-# FoundryVTT Module
-
-Does something, probably
-
-## Changelog
--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,207 @@
-Hooks.once('init', async function() {
+const MODULE_ID = 'elfreys-item-price';
 
+let EIPModuleSetting = {};
+EIPModuleSetting['ENABLED_PACKS'] = 'ENABLED_PACKS';
+EIPModuleSetting['PRICE_GEN_METHOD'] = 'priceGenMethod';
+EIPModuleSetting['ENABLE_SETTING'] = 'enableSetting';
+EIPModuleSetting['ITEM_PRICE_PACKS'] = 'itemPricePacks';
+
+const translate = (string) => {
+    return game.i18n.localize(string);
+};
+
+const setSetting = (setting, value) => {
+    return game.settings.set(MODULE_ID, setting, value);
+};
+const getSetting = (key) => {
+    return game.settings.get(MODULE_ID, key);
+};
+
+class PackSelect extends FormApplication {
+    static get defaultOptions() {
+        return {
+            ...super.defaultOptions,
+            title: '',
+            id: 'packs-select',
+            template: `modules/${MODULE_ID}/templates/packs-select.hbs`,
+            resizable: true,
+            width: 660,
+        };
+    }
+
+    getData() {
+        if (!game.packs) return null;
+        const enabledPacks = getSetting(EIPModuleSetting.ENABLED_PACKS);
+        const itemsPacks = game.packs.filter((pack) => {
+            return pack.metadata.type === 'Item';
+        });
+        const returnPacks = itemsPacks.map((pack) => {
+            return {
+                label: pack.metadata.label,
+                id: pack.metadata.id,
+                enabled: enabledPacks.includes(pack.metadata.id),
+            };
+        });
+        return {
+            compendiums: [...returnPacks],
+        };
+    }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+        // Set initial state for all
+        // const enabledPacks = getSetting(EIPModuleSetting.ENABLED_PACKS);
+        // Regular check change -> updates root check
+        html.find('.form-fields input').on('change', function () {
+            const compendium = this.id;
+            html.find(`[data-disable="${compendium}"]`).prop('checked', this.checked);
+        });
+        // Deselect all button
+        html.find('button.deselect-all').on('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            html
+                .find(`.form-group.pack input[type="checkbox"]`)
+                .prop('checked', false);
+        });
+        // Select all button
+        html.find('button.select-all').on('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            html
+                .find(`.form-group.pack input[type="checkbox"]`)
+                .prop('checked', true);
+        });
+    }
+
+    async _updateObject(event, formData) {
+        const enabledPacks = Object.keys(formData).filter(packId => formData[packId]);
+        console.log('enabledPacks', enabledPacks);
+        setSetting(EIPModuleSetting.ENABLED_PACKS, enabledPacks);
+    }
+}
+
+const priceByXGE = {
+    common: '(1d6+1) * 10',
+    uncommon: '(1d6) * 100',
+    rare: '2d10 * 1000',
+    veryRare: '(1d4+1) * 10000',
+    legendary: '2d6 * 25000',
+    artifact: '2d6 * 250000',
+};
+const priceByDMG = {
+    common: [50, 100],
+    uncommon: [101, 500],
+    rare: [501, 5000],
+    veryRare: [5001, 50000],
+    legendary: [50001, 2000000],
+    artifact: [2000000, 100000000],
+};
+
+const consumableTypes = ['potion', 'ammo', 'scroll'];
+
+function getRandomPriceByRange(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+Hooks.once('init', async () => {
+    const priceGenMethod = ['DMG', 'XGE'];
+
+    game.settings.registerMenu(MODULE_ID, EIPModuleSetting.ITEM_PRICE_PACKS, {
+        menu: 'itemPricePacksMenu',
+        name: translate('EIP.itemPricePacksName'),
+        label: translate('EIP.itemPricePacksLabel'),
+        hint: translate('EIP.itemPricePacksHint'),
+        icon: 'fas fa-search',
+        type: PackSelect,
+        restricted: false,
+    });
+    game.settings.register(MODULE_ID, EIPModuleSetting.ENABLE_SETTING, {
+        name: translate('EIP.moduleEnabledName'),
+        hint: translate('EIP.moduleEnabledHint'),
+        scope: 'world',
+        config: true,
+        type: Boolean,
+        default: true,
+    });
+    game.settings.register(MODULE_ID, EIPModuleSetting.PRICE_GEN_METHOD, {
+        name: translate('EIP.priceGenMethodName'),
+        hint: translate('EIP.priceGenMethodHint'),
+        type: Number,
+        default: 1,
+        choices: priceGenMethod,
+        scope: 'world',
+        config: true,
+        restricted: true,
+    });
+
+    game.settings.register(MODULE_ID, EIPModuleSetting.ENABLED_PACKS, {
+        name: translate('EIP.disabledPackName'),
+        type: Array,
+        default: [],
+        scope: 'world',
+        config: false,
+    });
 });
 
-Hooks.once('ready', async function() {
+const HALF_PRICE_MULTIPLIER = 0.5;
 
+const getPrice = async (
+    priceGenerationMethod,
+    diceRollFormula,
+    priceRange,
+    consumable = false
+) => {
+    const generatePrice = async () => {
+        if (priceGenerationMethod === 1) {
+            const rollPrice = new Roll(diceRollFormula);
+            return (await rollPrice.evaluate({ async: true })).total;
+        }
+        return getRandomPriceByRange(priceRange[0], priceRange[1]);
+    };
+
+    let price = await generatePrice();
+    const isConsumable = consumableTypes.includes(consumable);
+
+    if (isConsumable) {
+        price *= HALF_PRICE_MULTIPLIER;
+        console.log('Half price for consumable');
+    }
+
+    return price;
+};
+
+Hooks.on('createItem', async (item) => {
+    const sourcePack = item._stats.compendiumSource.split('.').slice(1, -2).join('.');
+    const isModuleEnabled = game.settings.get(MODULE_ID, EIPModuleSetting.ENABLE_SETTING);
+    if (!isModuleEnabled || item.system.price.value == null) return;
+
+    const enabledPacks = getSetting(EIPModuleSetting.ENABLED_PACKS);
+
+    let isFromEnabledPack = sourcePack && enabledPacks.includes(sourcePack);
+    
+
+    if (!isFromEnabledPack) return;
+
+    const priceGenMethod = game.settings.get(MODULE_ID, EIPModuleSetting.PRICE_GEN_METHOD);
+    const priceByRarity = priceGenMethod === 1 ? priceByXGE : priceByDMG;
+
+    updateItemPrice(item, priceGenMethod, priceByRarity);
 });
+
+async function updateItemPrice(item, priceGenMethod, priceByRarity) {
+    const rarity = item.system.rarity;
+    if (!rarity || !priceByRarity[rarity]) {
+        console.warning('No item rarity');
+        return;
+    }
+
+    const priceRange = priceByRarity[rarity];
+    const price = await getPrice(priceGenMethod, priceRange, [priceRange[0], priceRange[1]], item.system.consumableType);
+
+    item.update({
+        system: {
+            price: { value: price },
+        },
+    });
+}
